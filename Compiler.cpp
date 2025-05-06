a7833a323295d5606fd4a4947c1320718a02aa95
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-05-06 02:06:05 -0400

    Update Compiler.cpp

--- Compiler.cpp
+++ Compiler.cpp
@@ -1,1 +1,50 @@
-{scaffold_compiler}
+// --------------------------------------------
+// PARAGRAM COMPILER SCAFFOLD (C++ Style Pseudo)
+// --------------------------------------------
+
+class Token {
+    enum Type { IDENT, NUMBER, STRING, SYMBOL, KEYWORD };
+    Type type;
+    std::string value;
+};
+
+class Lexer {
+public:
+    std::vector<Token> tokenize(const std::string& code);
+};
+
+class ASTNode {
+public:
+    virtual void compile(NASMEmitter& out) = 0;
+};
+
+class Parser {
+    std::vector<Token> tokens;
+    int pos;
+public:
+    ASTNode* parseProgram();
+    ASTNode* parseStatement();
+    ASTNode* parseExpression();
+};
+
+class NASMEmitter {
+public:
+    void emit(const std::string& line);
+    void emitFunction(const std::string& name, std::vector<ASTNode*> body);
+    void emitAssignment(const std::string& var, const std::string& val);
+};
+
+int main(int argc, char** argv) {
+    std::string code = loadFile(argv[1]);
+    Lexer lexer;
+    auto tokens = lexer.tokenize(code);
+
+    Parser parser(tokens);
+    auto program = parser.parseProgram();
+
+    NASMEmitter emitter;
+    program->compile(emitter);
+
+    emitter.saveTo("out.asm");
+    return 0;
+}
